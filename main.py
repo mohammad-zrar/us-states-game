679a0be2f09110301d50e0333a8dfec695cc2aa3
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2023-01-04 12:55:46 +0300

    completed

---
+++ main.py
@@ -0,0 +1,55 @@
+import turtle
+import pandas
+import time
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+# ------------------- Getting x and y coordinate by using this code above. ---------------------------
+
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+#
+# turtle.mainloop()
+# --------------------------------------------
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name.").title()
+    if answer_state == "Exit":
+        # missing_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+        # Series.item(self) -> Return the first element of the underlying data as a python scalar.
+
+
+for state in missing_states:
+    time.sleep(0.5)
+    state_data = data[data.state == state]
+    t.goto(int(state_data.x), int(state_data.y))
+    t.write(state_data.state.item())
+
+screen.exitonclick()
